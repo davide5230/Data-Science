c79902a447970ffe88c612450194a931d034f17a
Davide Bottiglieri <davide5230@gmail.com>
2026-09-15 12:37:21 +0200

    Add files via upload

---
+++ Data/Dataset Analysis/wine-quality-analysis/requirements.R
@@ -0,0 +1,19 @@
+required_packages <- c(
+  "tidyverse",
+  "ggplot2",
+  "dplyr",
+  "GGally",
+  "FactoMineR",
+  "factoextra",
+  "lmtest",
+  "knitr"
+)
+
+installed_packages <- rownames(installed.packages())
+missing_packages <- required_packages[
+  !(required_packages %in% installed_packages)
+]
+
+if (length(missing_packages) > 0) {
+  install.packages(missing_packages)
+}
