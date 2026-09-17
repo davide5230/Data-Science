98d4166db002e9a41b910735dfcc2e733609825c
Davide Bottiglieri <davide5230@gmail.com>
2026-09-17 14:55:28 +0200

    Update processor.py

--- AI & Automation/AI-market-intelligence-monitor/src/processor.py
+++ AI & Automation/AI-market-intelligence-monitor/src/processor.py
@@ -15,3 +15,24 @@ def normalize_articles(articles):
         normalize_article(article)
         for article in articles
     ]
+
+def filter_articles(
+    articles,
+    language="en"
+):
+    filtered_articles = []
+
+    for article in articles:
+
+        if not article["title"]:
+            continue
+
+        if not article["description"]:
+            continue
+
+        if article["language"] != language:
+            continue
+
+        filtered_articles.append(article)
+
+    return filtered_articles
