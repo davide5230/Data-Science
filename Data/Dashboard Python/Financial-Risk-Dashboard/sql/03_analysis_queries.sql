92776a1f5c7afe2916dbd0c5eb83ff1eabee6186
Davide Bottiglieri <davide5230@gmail.com>
2026-09-22 12:19:23 +0200

    Update 03_analysis_queries.sql

--- Data/Dashboard Python/Financial-Risk-Dashboard/sql/03_analysis_queries.sql
+++ Data/Dashboard Python/Financial-Risk-Dashboard/sql/03_analysis_queries.sql
@@ -1,1 +1,27 @@
+-- Daily returns
 
+SELECT
+    ticker,
+    trade_date,
+    adjusted_close,
+
+    LAG(adjusted_close) OVER (
+        PARTITION BY ticker
+        ORDER BY trade_date
+    ) AS previous_close,
+
+    (
+        adjusted_close
+        /
+        LAG(adjusted_close) OVER (
+            PARTITION BY ticker
+            ORDER BY trade_date
+        )
+        - 1
+    ) AS daily_return
+
+FROM market_prices
+
+ORDER BY
+    ticker,
+    trade_date;
