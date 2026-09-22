6432dcaabfeff04988d6f2c9abc79aff02716a15
Davide Bottiglieri <davide5230@gmail.com>
2026-09-22 12:40:21 +0200

    Update 03_analysis_queries.sql

--- Data/Dashboard Python/Financial-Risk-Dashboard/sql/03_analysis_queries.sql
+++ Data/Dashboard Python/Financial-Risk-Dashboard/sql/03_analysis_queries.sql
@@ -23,3 +23,16 @@ SELECT
     ) AS daily_return
 
 FROM market_prices;
+
+-- Volatility
+
+SELECT
+    ticker,
+    AVG(daily_return) AS avg_daily_return,
+    STDDEV_SAMP(daily_return) AS daily_volatility,
+    STDDEV_SAMP(daily_return) * SQRT(252)
+        AS annualized_volatility
+FROM daily_returns
+WHERE daily_return IS NOT NULL
+GROUP BY ticker
+ORDER BY annualized_volatility DESC;
